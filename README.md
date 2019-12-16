0f2d8e0df7ed1962def82911e5e71b8c3f9f6e4a
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2019-12-16 13:34:59 +0100

    i got nothing but here is the start

---
+++ README.md
@@ -0,0 +1,15 @@
+#Rainbow console
+
+## How to install
+```bash
+npm i @singah/rainbow-console
+````
+
+jeg startede med at lave export dele fordi det ved jeg hvordan man gør
+efter det brugte jeg lang tid på at google
+    der fandt jeg ud af at hvis man skriver 
+        console.log("%cHey! this is a colored text", "color:blue");
+        så bliver texten blå, man kan også download en npm pakke som gør det for dig men det er ikke det vi skal gøre lige nu
+
+        på et andet stex på google fandt jeg ud af at farver har en underlig kode i node, så jeg lavede en array med de koder 
+        efter det var jeg fuldstændig lost så jeg gik tilbage til at google
---
+++ src/rainbow-console.js
@@ -0,0 +1,29 @@
+let colors = ["\x1b[30m", "\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m"];
+
+function rainbowConsole(){
+
+}
+
+module.exports = rainbowConsole;
+
+/* ---------------------------Brians version--------------------------------- */
+module.exports = [
+	"\x1b[31m", // red
+	"\x1b[33m", // yellow
+	"\x1b[32m", // green
+	"\x1b[36m", // cyan
+	"\x1b[34m", // blue
+	"\x1b[35m"  // magenta
+]
+
+function arrayIterator(symbol, index) {
+	if (symbol == " ") {
+		return " "
+	}
+	return `${colors[index % colors.length]}${symbol}\x1b[0m`
+}
+
+module.exports = function(message) {
+	let symbols = message.split("")
+	console.log(symbols.map(arrayIterator).join(""))
+}
